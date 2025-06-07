386840de9e78ff3f7e34d84b485ad2780cc8f33d
你的名字 <you@example.com>
2025-06-07 10:52:16 -0400

    增加一些测试的依赖

--- vite.config.ts
+++ vite.config.ts
@@ -10,11 +10,15 @@ export default defineConfig({
     visualizer({
       filename: './stats.html',
       open: true, // 自动打开浏览器
-    }),],
+    }),
+  ],
   resolve: {
-    alias: 
-    {
-        '@': path.resolve(__dirname, 'src'),
-    }
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    host: '0.0.0.0',  
+    port: 5173        
   }
 })
